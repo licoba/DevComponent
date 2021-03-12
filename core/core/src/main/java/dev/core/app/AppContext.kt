c1a6e2f595203c5f94ff02b99bca60031e1b07e4
Cjt <jtongttt@gmail.com>
2021-03-12 14:35:29 +0800

    1.新增部分依赖

--- core/core/src/main/java/dev/core/app/AppContext.kt
+++ core/core/src/main/java/dev/core/app/AppContext.kt
@@ -1,5 +1,6 @@
 package dev.core.app
 
+import android.util.Log
 import dev.DevUtils
 import dev.core.lib.base.BaseAppContext
 import dev.core.lib.engine.compress.LubanEngineImpl
@@ -17,7 +18,13 @@ import dev.engine.json.DevJSONEngine
 import dev.engine.log.DevLogEngine
 import dev.engine.media.DevMediaEngine
 import dev.engine.permission.DevPermissionEngine
+import dev.utils.LogPrintUtils
+import dev.utils.common.StringUtils
 
+/**
+ * detail: App Base Application
+ * @author Ttt
+ */
 open class AppContext : BaseAppContext() {
 
     override fun onCreate() {
@@ -25,7 +32,12 @@ open class AppContext : BaseAppContext() {
 
         // 初始化 DevUtils debug 相关配置
         if (AppDebug.isOpenDebug()) DevUtils.openDebug()
-        if (AppDebug.isOpenLog()) DevUtils.openLog()
+        // 是否打印日志
+        if (AppDebug.isOpenLog()) {
+            DevUtils.openLog()
+            // 日志拦截 ( 方便调试 )
+            devLogInterceptor()
+        }
 
         // 是否使用默认 Engine 配置
         if (isEngineConfig()) initializeEngine()
@@ -58,6 +70,29 @@ open class AppContext : BaseAppContext() {
         DevCompressEngine.setEngine(LubanEngineImpl())
         DevLogEngine.setEngine(object : DevLoggerEngineImpl() {
             override fun isPrintLog(): Boolean = AppDebug.isOpenDebug()
+        })
+    }
+
+    /**
+     * Dev 系列工具类日志拦截
+     */
+    private fun devLogInterceptor() {
+        // 可进行日志拦截编码
+        //DevLogger.setPrint(DevLogger.Print());
+        //JCLogUtils.setPrint(JCLogUtils.Print());
+        LogPrintUtils.setPrint(LogPrintUtils.Print { logType, tag, message -> // 防止 null 处理
+            var message: String? = message ?: return@Print
+            // 进行编码处理
+            message = StringUtils.strEncode(message, "UTF-8")
+            when (logType) {
+                Log.VERBOSE -> Log.v(tag, message)
+                Log.DEBUG -> Log.d(tag, message)
+                Log.INFO -> Log.i(tag, message)
+                Log.WARN -> Log.w(tag, message)
+                Log.ERROR -> Log.e(tag, message)
+                Log.ASSERT -> Log.wtf(tag, message)
+                else -> Log.wtf(tag, message)
+            }
         })
     }
 }
