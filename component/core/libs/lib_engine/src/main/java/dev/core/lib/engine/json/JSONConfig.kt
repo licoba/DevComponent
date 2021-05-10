21dbf5b688400db0c0d0ad1ef5912cca32167783
Cjt <jtongttt@gmail.com>
2021-05-10 10:13:46 +0800

    1.更新 engine kotlin 代码

---
+++ component/core/libs/lib_engine/src/main/java/dev/core/lib/engine/json/JSONConfig.kt
@@ -0,0 +1,13 @@
+package dev.core.lib.engine.json
+
+import com.google.gson.Gson
+import dev.engine.json.IJSONEngine
+
+/**
+ * detail: JSON Config
+ * @author Ttt
+ */
+class JSONConfig : IJSONEngine.EngineConfig() {
+    @JvmField
+    var gson: Gson? = null
+}
