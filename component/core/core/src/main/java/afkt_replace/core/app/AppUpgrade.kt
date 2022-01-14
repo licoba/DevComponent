7bfb26b7d7205da5c791fdc0194767c56570329c
Cjt <jtongttt@gmail.com>
2022-01-15 02:55:55 +0800

    1.更新部分代码

--- component/core/core/src/main/java/afkt_replace/core/app/AppUpgrade.kt
+++ component/core/core/src/main/java/afkt_replace/core/app/AppUpgrade.kt
@@ -1,8 +1,6 @@
 package afkt_replace.core.app
 
 import afkt_replace.core.BuildConfig
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 
 /**
  * detail: APP 渠道信息
@@ -20,18 +18,19 @@ object AppUpgrade {
  * detail: App 版本信息
  * @author Ttt
  */
-@Parcelize
-data class AppVersionInfo(
+class AppVersionInfo(
     val content: String? = "", // 版本更新内容
     val isForce: Boolean? = false, // 是否强制升级
-    val upgradeCode: Long = 0L, // 升级版本号
     val publishTimeMillis: Long = 0L, // 版本发布时间 ( 毫秒 )
     val channel: String? = "", // 渠道信息
     val versionCode: Int = 0, // APP 版本号
     val versionName: String? = "", // APP 版本名
-    var id: Long = 0L, // 版本升级数据 id
-    var fileMD5: String? = null, // APK MD5
-    var fileSHA256: String? = null, // APK SHA256
-    var fileUrl: String? = null, // APK 下载链接
-    var type: Int = 0, // 自定义字段 ( 拓展区分类型 )
-) : Parcelable
+    val id: Long = 0L, // 版本升级数据 id
+    val fileMD5: String? = null, // APK MD5
+    val fileSHA256: String? = null, // APK SHA256
+    val fileUrl: String? = null, // APK 下载链接
+    val type: Int = 0, // 自定义字段 ( 拓展区分类型 )
+    val upgradeCode: Long = 0L, // 升级版本号
+    val moduleId: Int = 0, // 模块 Id
+    val moduleName: String? = "" // 模块名
+)
