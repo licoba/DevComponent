158c0b10fe1b2ad668b0761ea75fe13daaf2fdd6
Cjt <jtongttt@gmail.com>
2022-01-17 23:12:40 +0800

    1.更新部分代码

--- component/core/libs/lib_environment/src/main/java/afkt_replace/core/lib/environment/IEnvironment.kt
+++ component/core/libs/lib_environment/src/main/java/afkt_replace/core/lib/environment/IEnvironment.kt
@@ -5,6 +5,9 @@ import android.content.Context
 /**
  * detail: APP 环境配置
  * @author Ttt
+ * 该类用于给 Core Module AppEnvironment 类实现
+ * 内部实现代码, 外部直接通过 AppEnvironment 进行调用无需关注内部实现逻辑
+ * 减少该模块暴露
  */
 interface IEnvironment {
 
@@ -12,6 +15,7 @@ interface IEnvironment {
      * 环境校验
      * @param context Context
      * @param index Int
+     * 需在 Application 内尽可能的早调用
      */
     fun checker(
         context: Context,
--- component/core/libs/lib_environment/src/main/java/afkt_replace/core/lib/environment/environment.kt
+++ component/core/libs/lib_environment/src/main/java/afkt_replace/core/lib/environment/environment.kt
@@ -10,6 +10,21 @@ import dev.environment.annotation.Module
  */
 internal class HttpService private constructor() {
 
+    @Module(alias = "登录、注册模块")
+    private inner class Login {
+        @Environment(value = "https://login-release.com", isRelease = true, alias = "生产环境")
+        private val release: String? = null
+
+        @Environment(value = "http://login-debug.com", alias = "测试环境")
+        private val debug: String? = null
+
+        @Environment(value = "http://login-pre_release.com", alias = "预发布环境")
+        private val pre_release: String? = null
+
+        @Environment(value = "http://login-development.com", alias = "开发环境")
+        private val development: String? = null
+    }
+
     @Module(alias = "用户模块")
     private inner class User {
         @Environment(value = "https://user-release.com", isRelease = true, alias = "生产环境")
@@ -25,18 +40,18 @@ internal class HttpService private constructor() {
         private val development: String? = null
     }
 
-    @Module(alias = "登录、注册模块")
-    private inner class Login {
-        @Environment(value = "https://login-release.com", isRelease = true, alias = "生产环境")
+    @Module(alias = "商品模块")
+    private inner class Commodity {
+        @Environment(value = "https://commodity-release.com", isRelease = true, alias = "生产环境")
         private val release: String? = null
 
-        @Environment(value = "http://login-debug.com", alias = "测试环境")
+        @Environment(value = "http://commodity-debug.com", alias = "测试环境")
         private val debug: String? = null
 
-        @Environment(value = "http://login-pre_release.com", alias = "预发布环境")
+        @Environment(value = "http://commodity-pre_release.com", alias = "预发布环境")
         private val pre_release: String? = null
 
-        @Environment(value = "http://login-development.com", alias = "开发环境")
+        @Environment(value = "http://commodity-development.com", alias = "开发环境")
         private val development: String? = null
     }
 }
@@ -48,9 +63,10 @@ internal class HttpService private constructor() {
 internal object EnvironmentTypeChecker {
 
     /**
-     * 环境校验
+     * 环境校验与重置
      * @param context Context
-     * @param index Int
+     * @param index Environment Type
+     * 需在 Application 内尽可能的早调用
      */
     fun checker(
         context: Context,
